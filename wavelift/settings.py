2c71bf80c2500f1eb5dfbb1a40a6ae0a53972b26
myselfanandvp <mailanandvp@gmail.com>
2025-09-06 10:14:52 +0530

    updated phone number error

--- wavelift/settings.py
+++ wavelift/settings.py
@@ -272,7 +272,7 @@ TIME_ZONE = "Asia/Kolkata"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 
